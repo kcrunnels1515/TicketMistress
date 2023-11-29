6d310bcdc115ad1dd8898c08fab52ad92b04fa4b
Kelly Laptop <kellyr@kelly-latitudee6430>
2023-11-29 17:12:57 -0500

    Completed ticket class

--- src/server/Ticket.hpp
+++ src/server/Ticket.hpp
@@ -1,6 +1,6 @@
 #ifndef TICKET_H_
 #define TICKET_H_
-
+#include <iostream>
 enum State {
   AL,AK, AZ, AR, CA, CO, CT, DE, FL, GA, HI,
   ID, IL, IN, IA, KS, KY, LA, ME, MD, MA, MI,
@@ -38,11 +38,33 @@ enum Color {
 
 };
 
+enum Make {
+  SEDAN,
+  SUV,
+  ELECTRIC,
+  MINIVAN,
+  PICKUP
+};
+
 class Ticket {
   private:
   State _state;
   Model _model;
   Color _color;
+  Make _make;
+  char* _time;
+
+  public:
+  Ticket(State state, Model model, Color color, Make make, std::string time)
+    : _state(state), _model(model), _color(color), _make(make) {
+    _time = new char[3];
+    _time[0] = (char) std::stoi(time.substr(0,2));
+    _time[1] = (char) std::stoi(time.substr(2,2));
+    _time[2] = (char) time[5];
+  }
+  ~Ticket() {
+    delete _time;
+  }
 };
 
 #endif // TICKET_H_
