558bb3ac0d347f70ded3071390dab77948960023
Kelly Laptop <kellyr@kelly-latitudee6430>
2023-11-28 20:17:39 -0500

    Added CSV parsing lib

---
+++ Makefile
@@ -0,0 +1,2 @@
+# needed for csv parser
+# g++ -std=c++0x a.o b.o -o prog -lpthread
---
+++ src/server/BTree.hpp
@@ -0,0 +1,10 @@
+#ifndef BTREE_HPP
+#define BTREE_HPP
+
+class BTree {
+  private:
+  public:
+  BTree();
+};
+
+#endif
