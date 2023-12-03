db19eac82b0e6ae5b856c70d232e74da4d882b27
UFmichael <michaelyao0315@gmail.com>
2023-12-03 13:05:43 -0500

    added front end files and the skeleton for bongo hash

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,16 +1,19 @@
-//
-// Created by micha on 12/1/2023.
-//
 #include <iostream>
 #include <string>
+#include "BongoHashMap.h"
 #include "Window.h"
 
 int main() {
     std::cout << "hello world";
-    Window test = Window();
-    vector<string> output;
-    output = test.start();
-    for(int i = 0; i < output.size(); i++){
-        std::cout << output[i] << std::endl;
-    }
+    BongoHashMap hashMap = BongoHashMap();
+    hashMap.addCase("AL", "ALPINA", "ORANGE", "SUBN", "1230");
+    hashMap.addCase("AK", "ALPINA", "ORANGE", "SUBN", "1230");
+    std::cout << "end of Data Structure";
+//    Window test = Window();
+//    vector<string> output;
+//    output = test.start();
+
+//    for(int i = 0; i < output.size(); i++){
+//        std::cout << output[i] << std::endl;
+//    }
 }
