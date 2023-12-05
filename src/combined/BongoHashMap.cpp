a0da4e80657d9ddf55f383194a7073205ba3dbb4
Kelly Laptop <kellyr@kelly-latitudee6430>
2023-12-04 20:46:49 -0500

    FINALLY

--- src/combined/BongoHashMap.cpp
+++ src/combined/BongoHashMap.cpp
@@ -10,12 +10,13 @@ BongoHashMap::BongoHashMap(){
 
 void BongoHashMap::resizeArr(){
     vector<Ticket*> temp_arr;
-    for (vector<Ticket*>& i : hashArray) {
+    for (vector<Ticket*> i : hashArray) {
         for (Ticket* t : i)
         temp_arr.push_back(t);
     }
     hashArray.clear();
     hashArray.reserve(capacity);
+    hashArray.assign(capacity, vector<Ticket*>());
     for (Ticket* t : temp_arr) {
         addCase(t);
     }
@@ -23,6 +24,7 @@ void BongoHashMap::resizeArr(){
 
 void BongoHashMap::addCase(Ticket* input_ticket){
     if((float)size/(float)capacity >= maxLoadFactor){
+        size = 0;
         capacity *= 2;
         cout << " Need to Resize" << endl;
         resizeArr();
@@ -69,41 +71,43 @@ vector<Ticket*> BongoHashMap::getCase(Ticket input_ticket){
 
 vector<Ticket*> BongoHashMap::getAllCases(Ticket input_ticket, unsigned char indep_vars){
     vector<Ticket*> vecContainer;
-    for (vector<Ticket*> v : hashArray) {
-        for (Ticket* i : v) {
-            bool stateBool = false, makeBool = false, modelBool = false,
-                colorBool = false, yearBool = false;
-            if ((indep_vars & 16) == 16) {
-                stateBool = true;
-            } else if (i->_state == input_ticket._state) {
-                stateBool = true;
-            }
+    for (int i = 0; i < (int) hashArray.size(); i++) {
+        if (!hashArray[i].empty()){
+            for (int j = 0; j < (int) hashArray[i].size(); j++) {
+                bool stateBool = false, makeBool = false, modelBool = false,
+                    colorBool = false, yearBool = false;
+                if ((indep_vars & 16) == 16) {
+                    stateBool = true;
+                } else if (hashArray[i][j]->_state == input_ticket._state) {
+                    stateBool = true;
+                }
 
-            if ((indep_vars & 8) == 8) {
-                makeBool = true;
-            } else if (i->_make == input_ticket._make) {
-                makeBool = true;
-            }
+                if ((indep_vars & 8) == 8) {
+                    makeBool = true;
+                } else if (hashArray[i][j]->_make == input_ticket._make) {
+                    makeBool = true;
+                }
 
-            if ((indep_vars & 4) == 4) {
-                modelBool = true;
-            } else if (i->_model == input_ticket._model) {
-                modelBool = true;
-            }
+                if ((indep_vars & 4) == 4) {
+                    modelBool = true;
+                } else if (hashArray[i][j]->_model == input_ticket._model) {
+                    modelBool = true;
+                }
 
-            if ((indep_vars & 2) == 2) {
-                colorBool = true;
-            } else if (i->_color == input_ticket._color) {
-                colorBool = true;
-            }
+                if ((indep_vars & 2) == 2) {
+                    colorBool = true;
+                } else if (hashArray[i][j]->_color == input_ticket._color) {
+                    colorBool = true;
+                }
 
-            if ((indep_vars & 1) == 1) {
-                yearBool = true;
-            } else if (i->_state == input_ticket._state) {
-                yearBool = true;
-            }
-            if (stateBool && makeBool && modelBool && colorBool && yearBool) {
-                vecContainer.push_back(i);
+                if ((indep_vars & 1) == 1) {
+                    yearBool = true;
+                } else if (hashArray[i][j]->_year == input_ticket._year) {
+                    yearBool = true;
+                }
+                if (stateBool && makeBool && modelBool && colorBool && yearBool) {
+                    vecContainer.push_back(hashArray[i][j]);
+                }
             }
         }
     }
@@ -112,8 +116,14 @@ vector<Ticket*> BongoHashMap::getAllCases(Ticket input_ticket, unsigned char ind
 
 pair<float, vector<Ticket*>> BongoHashMap::getStats(Ticket input_ticket, unsigned char indep_vars) {
     vector<Ticket*> other = getAllCases(input_ticket, indep_vars);
-    vector<Ticket*> other_top = vector<Ticket*>(other.begin(), other.begin() + 8);
-    return make_pair((float) getCase(input_ticket).size() / (float) other.size(), other_top);
+    vector<Ticket*> other_top;
+    if (!other.empty()) {
+        for (int i = 0; i < 8; i++){
+            other_top.push_back(other[i]);
+        }
+    }
+    float prob = (float) getCase(input_ticket).size() / (float) other.size();
+    return make_pair(prob, other_top);
 };
 
 int BongoHashMap::hashFunction(Ticket input_ticket){
